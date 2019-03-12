31e7a81b4f262692e39c665f5ee07766c07f1120
Tai Kedzierski <dch.tai@gmail.com>
2019-03-12 21:46:03 +0000

    change example dirty to spaces, fix target detection

--- examples/webserver-dirty.sh
+++ examples/webserver-dirty.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
 util:killconn() {
-	kill -9 "$conn_id"
+    kill -9 "$conn_id"
 }
 trap util:killconn EXIT
 
 while true; do
-	input="$(mktemp)"
-	output="$(mktemp)"
-	pidfile="$(mktemp)"
+    input="$(mktemp)"
+    output="$(mktemp)"
+    pidfile="$(mktemp)"
 
-	tail -f "$output" | nc -l 8080 > "$input" &
-	conn_id="$!"
+    tail -f "$output" | nc -l 8080 > "$input" &
+    conn_id="$!"
 
-	echo "Listening process: $conn_id"
-	while ! [[ "$(grep -Pc ^ "$input")" -gt 0 ]] ; do
-		sleep 2
-	done
+    echo "Listening process: $conn_id"
+    while ! [[ "$(grep -Pc ^ "$input")" -gt 0 ]] ; do
+        sleep 2
+    done
 
-	echo "Got a connection $(date)"
+    echo "Got a connection $(date)"
 
-	[[ "$(head -n 1 "$input")" =~ GET\ ([^ ]+) ]]
+    [[ "$(head -n 1 "$input")" =~ GET\ ([^ ]+) ]]
+    target="${BASH_REMATCH[1]}"
 
-	cat "$input"
+    cat "$input"
 
-	if [[ -f "$target" ]]; then
-		echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: $(stat --printf="%s" "$target")\r\n\r\n" >> "$output"
-		cat "$target" >> "$output"
-	else	
-		echo -e "HTTP/1.1 400 No good\r\nContent-Type: text/plain\r\nContent-Length: 21\r\n\r\nFile not found!" >> "$output"
-	fi
+    if [[ -f "$target" ]]; then
+        echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: $(stat --printf="%s" "$target")\r\n\r\n" >> "$output"
+        cat "$target" >> "$output"
+    else    
+        echo -e "HTTP/1.1 400 No good\r\nContent-Type: text/plain\r\nContent-Length: 21\r\n\r\nFile not found!" >> "$output"
+    fi
 
-	rm "$input" "$output" "$pidfile"
-	kill -9 "$conn_id"
+    rm "$input" "$output" "$pidfile"
+    kill -9 "$conn_id"
 done
